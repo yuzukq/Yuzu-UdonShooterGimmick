99922094133e5b85e013b8eaa90f27878c8c68c6
yuzukq <yuzupanda1006@icloud.com>
2025-01-18 21:34:14 +0900

    ビルドエラーにつきバックアップ用push

--- script/MagEquip.cs
+++ script/MagEquip.cs
@@ -1,13 +1,34 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class MagEquip : UdonSharpBehaviour
 {
-    void Start()
+    [SerializeField] Transform MagagineBelt;
+    bool IsEquiped = false;
+
+    public override void Interact()
     {
-        
+        IsEquiped = !IsEquiped;
+    }   
+    
+    void Update()
+    {
+        if(!Utilities.IsValid(Networking.LocalPlayer))
+        { 
+            return; //エラー回避
+        }   
+        if(IsEquiped == false) 
+        { 
+            return; 
+        }
+ 
+        MagagineBelt.position = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Chest);
+        Vector3 XVector = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.RightShoulder) - Networking.LocalPlayer.GetBonePosition(HumanBodyBones.LeftShoulder);
+        Vector3 YVector = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Neck) - Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Hips);
+        Vector3 ZVector = Vector3.Cross(XVector, YVector);
+        MagagineBelt.transform.rotation = Quaternion.LookRotation(ZVector, YVector);
     }
 }
+

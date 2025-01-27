997844a8ca143adf2504d5286c7249f30d02e645
yuzukq <yuzupanda1006@icloud.com>
2025-01-28 00:42:26 +0900

    物理スライダー,UIスライダーの実装

---
+++ script/MagScaleSlider.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class MagScaleSlider : UdonSharpBehaviour
+{
+    [SerializeField] private Slider beltSizeSlider;
+    [SerializeField] private Transform MagagineBelt;
+    void Start()
+    {
+        UpdateBeltSize();
+    }
+
+    public void UpdateBeltSize()
+    {
+        float scale = beltSizeSlider.value;
+        MagagineBelt.localScale = Vector3.one * scale;
+    }
+}
---
+++ script/UpGradeUILever.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class UpGradeUILever : UdonSharpBehaviour
+{
+    [SerializeField] private Transform handle;
+    
+    [SerializeField] private Transform upGradeUI; 
+
+    [SerializeField] private float minX = 0f; //レールの開始位置
+    [SerializeField] private float maxX = 0.5f; //レールの終了位置
+
+    private void Update()
+    {
+        // 持ち手のローカル位置を取得
+        Vector3 handleLocalPosition = handle.localPosition;
+        handle.localPosition = handleLocalPosition;
+        handleLocalPosition.x = Mathf.Clamp(handleLocalPosition.x, minX, maxX);
+
+        
+        handleLocalPosition.y = 0f; //軸固定
+        handleLocalPosition.z = 0f; 
+        handle.localRotation = Quaternion.identity; //回転軸を(0, 0, 0)に固定
+
+        //基準点からの移動量でUpGradeUIのスケールを変更
+        float scaleValue = Mathf.InverseLerp(minX, maxX, handleLocalPosition.x);
+        upGradeUI.localScale = Vector3.one * scaleValue;
+    }
+}

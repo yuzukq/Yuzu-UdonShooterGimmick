1e3b763523c0bfb19b816d789649b663ec7a1619
yuzukq <yuzupanda1006@icloud.com>
2025-04-13 20:22:50 +0900

    公開班管理対象ファイルを更新

---
+++ script/SweeperGameManager.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class SweeperGameManager : UdonSharpBehaviour
+{
+    [Header("ゲーム設定")]
+    [SerializeField] float playTime = 300f;
+    [SerializeField] int sweeperCount = 0;
+    [SerializeField] int sweeperCountMax = 120;
+    
+    [Header("スイーパープレハブ設定")]
+    [SerializeField] GameObject Sweeper001;
+    [SerializeField] GameObject Sweeper002;
+    [SerializeField] GameObject Sweeper003;
+
+    private int waveCount = 0;
+    private float waveTime = 0f;
+    
+    
+}
